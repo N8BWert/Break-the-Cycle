d93cb9d62616a17322b1ba26b1222733805b8252
Nathaniel Wert <39998614+N8BWert@users.noreply.github.com>
2020-10-04 07:02:41 -0400

    Made Main Menu
    I made a quick main menu, but now I'm not getting sound so I'm going to debug that.

---
+++ Assets/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public string nextScene;
+    private AudioSource source;
+    public AudioClip pop;
+
+    void Start() {
+        source = GetComponent<AudioSource>();
+    }
+    public void OnClick() {
+        source.PlayOneShot(pop);
+        SceneManager.LoadScene(nextScene);
+    }
+}
